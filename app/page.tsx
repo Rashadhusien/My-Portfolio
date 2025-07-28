2c1f98d00ba5d95cf51a9e69e1217ef5aaad49a0
Rashadhusien <rashadhusein440@gmail.com>
2025-07-29 00:11:14 +0300

    hide Testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           <Hero />
           <About />
           <Projects />
-          <Testimonials />
+          {/* <Testimonials /> */}
           {/* <Blog /> */}
           <Contact />
         </main>
